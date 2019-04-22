38a96a43fb9623b80eee124e67ebe3edadf141c1
Corinne <corinnegottlieb@gmail.com>
2019-04-22 17:22:32 +0300

    code review fixes to spot checks 1 -2

--- tests/spot-check-1/spot-check-1.test.js
+++ tests/spot-check-1/spot-check-1.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../src/App';
 import Adapter from 'enzyme-adapter-react-16';
-import { MemoryRouter } from 'react-router-dom';
 import { mount, configure} from 'enzyme';
 import Company from '../../src/components/Company';
 
@@ -11,24 +10,26 @@ configure({ adapter: new Adapter() });
 describe("spotcheck1", () => {
     it('Application should render without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
+        ReactDOM.render(<App />, div);
         ReactDOM.unmountComponentAtNode(div);
       });    
       it('You must render an h4 element on the page with the word Tesla', () => {
         const wrapper = mount(<App />);
         const expectedText = "Tesla";
-        let text = wrapper.find('h4')
-        expect(text.exists(), 'could not find h4 element').toBeTruthy()
-        const actualText = text.first().text();
-        expect(text.first().text(), `The text in the h4 should be '${expectedText}'. Instead, we found ${actualText}`).toBe(expectedText)
         let spotcheck1Div= wrapper.find('#spotcheck-1')
-        expect(spotcheck1Div.children(), "You should only render one h4 element").toHaveLength(2)
+        let teslaText = spotcheck1Div.find('h4')
+        expect(teslaText.exists(), 'could not find h4 element').toBeTruthy()
+        const actualText = teslaText.first().text();
+        expect(actualText, `The text in the h4 should be '${expectedText}'. Instead, we found '${actualText}'`).toBe(expectedText)
+        expect(teslaText, "You should only render one h4 element").toHaveLength(1)
       });
       it('The App component should render the Company component with props', () => {
         const wrapper = mount(<App />);
         let companyComponent = wrapper.find(Company);
         expect(companyComponent.exists(), 'You must create a Component called Company').toBeTruthy()
-        expect(companyComponent.first().props(), 'props were not passed accurately').toEqual({ name: 'Tesla', revenue: 140})
+        const expectedProps = { name: 'Tesla', revenue: 140}
+        const actualProps = companyComponent.first().props()
+        expect(actualProps, `props were not passed accurately; expected ${JSON.stringify(expectedProps)}, instead found ${JSON.stringify(actualProps)}`).toEqual(expectedProps)
     })
     it('The Company component should render the SubCompany component', () => {
         const wrapper = mount(<Company name='Tesla' />);
@@ -36,7 +37,6 @@ describe("spotcheck1", () => {
         expect(subCompanyComponent.exists(), 'You must create a Component called SubCompany').toBeTruthy()
         expect(wrapper, 'The Company component must render the SubCompany component').toHaveLength(1);
     })
-
 })
 
 
--- tests/spot-check-2/spot-check-2.test.js
+++ tests/spot-check-2/spot-check-2.test.js
@@ -2,27 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../src/App';
 import Adapter from 'enzyme-adapter-react-16';
-import { MemoryRouter } from 'react-router-dom';
 import { mount, configure } from 'enzyme';
-import Company from '../../src/components/Company';
 
 configure({ adapter: new Adapter() });
 
 describe("spotcheck2", () => {
   it('Application should render without crashing', () => {
     const div = document.createElement('div');
-    // remove memoryrouter
-    ReactDOM.render(<MemoryRouter><App /></MemoryRouter>, div);
+    ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
   it('You must render an h4 element on the page for each Comppany', () => {
     const wrapper = mount(<App />);
     let spotcheck2Div = wrapper.find('#spotcheck-2')
-    expect(spotcheck2Div.children(), 'There should be three h4 elements rendered on the page').toHaveLength(4)
-    expect(spotcheck2Div.children().at(1).text(), "The first h4 should have the text 'Tesla'").toBe('Tesla')
-    expect(spotcheck2Div.children().at(2).text(), "The second h4 should have the text 'Microsoft'").toBe('Microsoft')
-    expect(spotcheck2Div.children().at(3).text(), "The third h4 should have the text 'Google'").toBe('Google')
-
+    let companies = spotcheck2Div.find('h4')
+    expect(companies, 'There should be three h4 elements rendered on the page').toHaveLength(3)
+    const expected1 = 'Tesla'
+    const actual1 = companies.first().text()
+    expect(actual1, `The first h4 should have the text '${expected1}'; instead found '${actual1}'`).toBe(expected1)
+    const expected2 = 'Microsoft'
+    const actual2 = companies.at(1).text()
+    expect(actual2, `The second h4 should have the text '${expected2}'; instead found '${actual2}`).toBe(expected2)
+    const expected3 = 'Google'
+    const actual3 = companies.at(2).text()
+    expect(actual3, `The third h4 should have the text '${expected3}'; instead found '${actual3}`).toBe(expected3)
   });
   it("The App component's render function should invoke the generateCompanyTags function", () => {
     expect(App.prototype.generateCompanyTags, 'You must define the generateCompanyTags method in your App component').toBeDefined()
@@ -31,12 +34,6 @@ describe("spotcheck2", () => {
     }
     const wrapper = mount(<App />);
     let spotcheckMock = wrapper.find('#spotcheck-2')
-    expect(spotcheckMock.children(), "generateCompanyTags must be returned in your App's render function").toHaveLength(2)
+    expect(spotcheckMock.children('div'), "generateCompanyTags must be returned in your App's render function").toHaveLength(1)
   })
 })
-
-
-
-
-
-
