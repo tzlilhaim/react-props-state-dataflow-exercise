543b7b78d649c4aaf52fe3fc9c77a4dadaa2eef7
jona <jona@elevation.ac>
2019-04-29 00:53:20 +0300

    loosened ex2

--- tests/exercises/ex-2.test.js
+++ tests/exercises/ex-2.test.js
@@ -28,22 +28,23 @@ describe("exercise2", () => {
     it("The Landing component should render a div which says 'Welcome, Robyn', followed by the hottest item", () => {
         const wrapper = mount(<Landing user="mockUser" store={[{item: "mockItem", price: 3099, discount: 0.05, hottest: true}]} />);
         let div = wrapper.find('div')
-        let text = div.text()
-        expect(div, 'could not find a div rendered on the page').toHaveLength(1)
-        expect(text, "could not find the text 'Welcome' rendered by Landing component").toContain('Welcome')
-        expect(text, "the user's name should be passed as props to the Landing component from the App's state").toContain(wrapper.props().user)
+        let text = wrapper.text()
+        expect(div.length, 'could not find a div rendered on the page').toBeGreaterThan(0)
+        expect(text, "the user's name should be passed as props to the Landing component from the App's state").toContain("mockUser")
         expect(text, "the hottest item should be rendered by accessing props passed from the Apps's state").toContain('mockItem')
     })
+
     it("The Home component should render the Item component for every item in the store", () => {
         const wrapper = mount(<Home store={[{item: "mockItem", price: 3099, discount: 0.05, hottest: true}]}/>);
         let itemComponent = wrapper.find(Item)
         expect(itemComponent.exists(), 'could not find an Item component in the Home component').toBeTruthy()
     });
+
     it("The Item component should render a div with the name and price of an item", () => {
         const wrapper = mount(<Item name="mockName" price="mockPrice"/>);
-        let itemDiv = wrapper.find('div')
+        let itemDiv = wrapper.find(Item)
         expect(itemDiv.exists(), 'could not find a div rendered by the Item component').toBeTruthy()
-        expect(itemDiv.text(), 'the name of the item should be passed using props').toContain(wrapper.props().name)
-        expect(itemDiv.text(), 'the price of the item sould be passed using props').toContain(wrapper.props().price)
+        expect(itemDiv.text(), 'the name of the item should be passed using props').toContain("mockName")
+        expect(itemDiv.text(), 'the price of the item sould be passed using props').toContain("mockPrice")
     });
 })
