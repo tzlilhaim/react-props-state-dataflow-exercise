830f998181a17c338c4248fc87f821d5d0009fd7
jona <jona@elevation.ac>
2019-04-28 21:06:10 +0300

    clarify sc3

--- tests/spot-check-3/spot-check-3.test.js
+++ tests/spot-check-3/spot-check-3.test.js
@@ -14,7 +14,7 @@ describe("spotcheck3", () => {
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  it("You must render an h4 element on the page for each Comppany, containing the company's name in capital letters", () => {
+  it("You must render an h4 element on the page for each Company, containing the company's name in capital letters", () => {
     const wrapper = mount(<App />);
     let spotcheck3Div = wrapper.find('#spotcheck-3')
     let company = spotcheck3Div.find('h4')
@@ -37,7 +37,7 @@ describe("spotcheck3", () => {
     const wrapper = mount(<App />);
     let spotcheckMock = wrapper.find('#spotcheck-3')
     let companyComponent = spotcheckMock.find(Company);
-    expect(companyComponent.exists(), 'You must render a Component called Company').toBeTruthy()
+    expect(companyComponent.exists(), 'You must render a Component called Company inside of #spotcheck-3').toBeTruthy()
     expect(companyComponent.first().props(), 'props were not passed with the upperCase function').toEqual({name: "mock", revenue: 140})
     expect(companyComponent.at(1).props(), 'props were not passed with the upperCase function').toEqual({name: "mock", revenue: 300})
     expect(companyComponent.at(2).props(), 'props were not passed with the upperCase function').toEqual({name: "mock", revenue: 600})
